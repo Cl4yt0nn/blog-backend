bc88afcf2546a05d3ebdebad6f3da6df195c4a22
Cl4yt0nn <111470334+Cl4yt0nn@users.noreply.github.com>
2023-11-14 12:32:54 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use("/users", userRoute);
 app.use("/posts", blogRoute);
 
 // PORT
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log("Connected to port", port);
 });
